cdcef5bbdf74c17665366b539d7656a78b0b5c48
KimJY <kjy3501@naver.com>
2022-09-16 12:29:08 +0900

    page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-window.onload = function(){
-  
+window.onload = function () {
+  const lifeSw = new Swiper('.life-sw', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 0,
+    centeredSlides: true,
+    navigation: {
+      prevEl: '.life-sw-prev',
+      nextEl: '.life-sw-next'
+    },
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+  })
 }
