fb3325e27bfcc8a9ec56c534038f2fb6b11e2a1a
devjy1550 <kjy3501@naver.com>
2022-09-18 18:13:16 +0900

    수정중

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
-window.onload = function(){
-  
+window.onload = function () {
+  new Swiper('.life-sw', {
+    loop: true,
+    slidesPerView: 3,
+    navigation: {
+      prevEl: '.life-sw-prev',
+      nextEl: '.life-sw-next'
+    },
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false,
+    },
+  })
 }
