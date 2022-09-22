fb0cc7da3146fb662b95af7ba90cfc2fa1b3735f
KimJY <kjy3501@naver.com>
2022-09-22 17:54:50 +0900

    수정중

--- js/script.js
+++ js/script.js
@@ -1,286 +1,253 @@
 window.onload = function () {
-  new Swiper('.life-sw', {
+  new Swiper(".life-sw", {
     loop: true,
     slidesPerView: 3,
     spaceBetween: 0,
     centeredSlides: true,
     navigation: {
-      prevEl: '.life-sw-prev',
-      nextEl: '.life-sw-next'
+      prevEl: ".life-sw-prev",
+      nextEl: ".life-sw-next",
     },
     autoplay: {
       delay: 2000,
       disableOnInteraction: false,
     },
-  })
+  });
 
-  new Swiper('.list-sw', {
-    speed: 1000,
-    direction : 'vertical',
-    allowTouchMove: true,
-    slidesPerView:2
-  })
-
-
-
-
-  let psSW = new Swiper('.personal-swiper', {
+  const plSW = new Swiper(".portlist-sw", {
     loop: true,
-    autoplay: {
-      delay: 2000,
-      disableOnInteraction: false,
+    speed: 2000,
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "3",
+    coverflowEffect: {
+      rotate: 40,
+      stretch: 40,
+      depth: 100,
+      modifier: 1,
     },
-    pauseOnMouseEnter: true,
-    navigation: {
-      prevEl: '.psn-sw-prev',
-      nextEl: '.psn-sw-next'
-    },
-    pagination: {
-      type: 'bullets',
-      el: '.psn-sw-pgnt',
-    },
+  });
 
-  })
-  const stSW = new Swiper('.study-swiper', {
-    speed: 1000,
-    loop: false,
-    allowTouchMove: true,
-    navigation: {
-      prevEl: '.st-sw-prev',
-      nextEl: '.st-sw-next'
-    },
-    pagination: {
-      type: 'bullets',
-      el: '.st-sw-pgnt',
-    },
-    resistanceRatio	: 0
-  })
-
-  const stttSW = new Swiper('.st-title-sw', {
-    speed: 1000,
-    loop: false,
+  const stttSW = new Swiper(".st-title-sw", {
+    speed: 2000,
+    loop: true,
     allowTouchMove: false,
-  })
+  });
 
-
-  stSW.controller.control = stttSW;
-
+  plSW.controller.control = stttSW;
 
   var bar = new ProgressBar.Circle(html, {
-    color: '#ff3f34',
-    trailColor: '#E9F8F0',
+    color: "#ff3f34",
+    trailColor: "#E9F8F0",
     trailWidth: 1,
     duration: 2500,
-    easing: 'bounce',
+    easing: "bounce",
     strokeWidth: 12,
     from: {
-      color: '#FFE6D6',
-      a: 0
+      color: "#FFE6D6",
+      a: 0,
     },
     to: {
-      color: '#ff3f34',
-      a: 1
+      color: "#ff3f34",
+      a: 1,
     },
     // Set default step function for all animate calls
     step: function (state, circle) {
-      circle.path.setAttribute('stroke', state.color);
+      circle.path.setAttribute("stroke", state.color);
 
       var value = Math.round(circle.value() * 98);
       if (value === 0) {
-        circle.setText('');
+        circle.setText("");
       } else {
         circle.setText(value);
       }
-    }
+    },
   });
   bar.animate(0.98);
 
   var bar2 = new ProgressBar.Circle(scss, {
-    color: '#ffd32a',
-    trailColor: '#E9F8F0',
+    color: "#ffd32a",
+    trailColor: "#E9F8F0",
     trailWidth: 1,
     duration: 2500,
-    easing: 'bounce',
+    easing: "bounce",
     strokeWidth: 12,
     from: {
-      color: '#FFF9D4',
-      a: 0
+      color: "#FFF9D4",
+      a: 0,
     },
     to: {
-      color: '#ffd32a',
-      a: 1
+      color: "#ffd32a",
+      a: 1,
     },
     // Set default step function for all animate calls
     step: function (state, circle) {
-      circle.path.setAttribute('stroke', state.color);
+      circle.path.setAttribute("stroke", state.color);
       var value = Math.round(circle.value() * 100);
       if (value === 0) {
-        circle.setText('');
+        circle.setText("");
       } else {
         circle.setText(value);
       }
-    }
+    },
   });
   bar2.animate(0.91);
 
   var bar3 = new ProgressBar.Circle(js, {
-    color: '#04c56b',
-    trailColor: '#E9F8F0',
+    color: "#04c56b",
+    trailColor: "#E9F8F0",
     trailWidth: 1,
     duration: 2500,
-    easing: 'bounce',
+    easing: "bounce",
     strokeWidth: 12,
     from: {
-      color: '#CAFCD1',
-      a: 0
+      color: "#CAFCD1",
+      a: 0,
     },
     to: {
-      color: '#04c56b',
-      a: 1
+      color: "#04c56b",
+      a: 1,
     },
     // Set default step function for all animate calls
     step: function (state, circle) {
-      circle.path.setAttribute('stroke', state.color);
+      circle.path.setAttribute("stroke", state.color);
       var value = Math.round(circle.value() * 100);
       if (value === 0) {
-        circle.setText('');
+        circle.setText("");
       } else {
         circle.setText(value);
       }
-    }
+    },
   });
   bar3.animate(0.9);
 
-
   var bar4 = new ProgressBar.Circle(vue, {
-    color: '#00d8d6',
-    trailColor: '#E9F8F0',
+    color: "#00d8d6",
+    trailColor: "#E9F8F0",
     trailWidth: 1,
     duration: 2500,
-    easing: 'bounce',
+    easing: "bounce",
     strokeWidth: 12,
     from: {
-      color: '  #CAFDEA',
-      a: 0
+      color: "  #CAFDEA",
+      a: 0,
     },
     to: {
-      color: '#00d8d6',
-      a: 1
+      color: "#00d8d6",
+      a: 1,
     },
     // Set default step function for all animate calls
     step: function (state, circle) {
-      circle.path.setAttribute('stroke', state.color);
+      circle.path.setAttribute("stroke", state.color);
       var value = Math.round(circle.value() * 100);
       if (value === 0) {
-        circle.setText('');
+        circle.setText("");
       } else {
         circle.setText(value);
       }
-    }
-
+    },
   });
   bar4.animate(0.9);
 
-
   var bar5 = new ProgressBar.Circle(git, {
-    color: '#3d40c6',
-    trailColor: '#E9F8F0',
+    color: "#3d40c6",
+    trailColor: "#E9F8F0",
     trailWidth: 1,
     duration: 2500,
-    easing: 'bounce',
+    easing: "bounce",
     strokeWidth: 12,
     from: {
-      color: '#D9DAFC',
-      a: 0
+      color: "#D9DAFC",
+      a: 0,
     },
     to: {
-      color: '#3d40c6',
-      a: 1
+      color: "#3d40c6",
+      a: 1,
     },
     // Set default step function for all animate calls
     step: function (state, circle) {
-      circle.path.setAttribute('stroke', state.color);
+      circle.path.setAttribute("stroke", state.color);
       var value = Math.round(circle.value() * 100);
       if (value === 0) {
-        circle.setText('');
+        circle.setText("");
       } else {
         circle.setText(value);
       }
-    }
+    },
   });
   bar5.animate(0.92);
 
   var bar6 = new ProgressBar.Circle(bootstrap, {
-    color: '#694fb6',
-    trailColor: '#E9F8F0',
+    color: "#694fb6",
+    trailColor: "#E9F8F0",
     trailWidth: 1,
     duration: 2500,
-    easing: 'bounce',
+    easing: "bounce",
     strokeWidth: 12,
     from: {
-      color: '#E9DEFB',
-      a: 0
+      color: "#E9DEFB",
+      a: 0,
     },
     to: {
-      color: '#694fb6',
-      a: 1
+      color: "#694fb6",
+      a: 1,
     },
     // Set default step function for all animate calls
     step: function (state, circle) {
-      circle.path.setAttribute('stroke', state.color);
+      circle.path.setAttribute("stroke", state.color);
       var value = Math.round(circle.value() * 100);
       if (value === 0) {
-        circle.setText('');
+        circle.setText("");
       } else {
         circle.setText(value);
       }
-    }
+    },
   });
   bar6.animate(0.94);
 
   var bar7 = new ProgressBar.Circle(gf, {
-    color: '#48545f',
-    trailColor: '#E9F8F0',
+    color: "#48545f",
+    trailColor: "#E9F8F0",
     trailWidth: 1,
     duration: 2500,
-    easing: 'bounce',
+    easing: "bounce",
     strokeWidth: 12,
     from: {
-      color: '#EBF3F7',
-      a: 0
+      color: "#EBF3F7",
+      a: 0,
     },
     to: {
-      color: '#48545f',
-      a: 1
+      color: "#48545f",
+      a: 1,
     },
     // Set default step function for all animate calls
     step: function (state, circle) {
-      circle.path.setAttribute('stroke', state.color);
+      circle.path.setAttribute("stroke", state.color);
       var value = Math.round(circle.value() * 100);
       if (value === 0) {
-        circle.setText('');
+        circle.setText("");
       } else {
         circle.setText(value);
       }
-    }
+    },
   });
   bar7.animate(0.93);
 
-
-  let skillList = $('.skill-list >li >a');
-  let skillDesc = $('.skilldesc-box >.skilldesc');
+  let skillList = $(".skill-list >li >a");
+  let skillDesc = $(".skilldesc-box >.skilldesc");
   $.each(skillList, function (index) {
     $(this).mouseenter(function () {
-      skillDesc.eq(index).addClass('skilldesc-active');
-    })
+      skillDesc.eq(index).addClass("skilldesc-active");
+    });
     $(this).mouseleave(function () {
-      skillDesc.removeClass('skilldesc-active');
-    })
+      skillDesc.removeClass("skilldesc-active");
+    });
     $(this).mousemove(function (e) {
       let x = e.clientX;
       let y = e.clientY;
-      skillDesc.css('left', x + 5);
-      skillDesc.css('top', y + 5);
-    })
-  })
-}
+      skillDesc.css("left", x + 5);
+      skillDesc.css("top", y + 5);
+    });
+  });
+};
